96f58d2d093579a4ec138c9a2acd6f5f17ce9280
sachinpatgar11 <sachinpatgar39@gmail.com>
2025-04-07 17:51:41 +0530

    updating modal to show the cart data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import Cart from "./components/Cart.jsx";
 import Header from "./components/Header.jsx";
 import Meals from "./components/Meals.jsx";
 import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
 
 function App() {
   return (
-    <CartContextProvider>
-      <Header></Header>
-      <Meals />
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header></Header>
+        <Meals />
+        <Cart />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,19 @@ import React, { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button.jsx";
 import CartContext from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 function Header() {
   const cartctx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
   const totalCartItems = cartctx.items.reduce((totalNumberOfItem, item) => {
     return totalNumberOfItem + item.quantity;
   }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
 
   return (
     <header id="main-header">
@@ -16,7 +23,9 @@ function Header() {
         <h1>REACTFOOD</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
